43cbf3a9e77caf948de44008314efd6a8cf242b7
MS.Konovalov <konovalov.ms93@gmail.com>
2019-12-20 10:47:31 +0300

    App key reading added

--- src/main/java/Debug.java
+++ src/main/java/Debug.java
@@ -1,6 +1,9 @@
 import core.ItemsSeeker;
 import core.Logger;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
@@ -11,9 +14,9 @@ public class Debug implements Logger {
 
     private static String[] testQueries = new String[]{"spinning reel shimano", "watches"};
 
-    public static void main(String[] args) {
-        itemsSeeker = new ItemsSeeker(Arrays.asList(testQueries), ItemsSeeker.Condition.NOT_SPEC);
-
+    public static void main(String[] args) throws IOException {
+        String key = Files.readAllLines(Paths.get("key.txt")).get(0);
+        itemsSeeker = new ItemsSeeker(Arrays.asList(testQueries), ItemsSeeker.Condition.NOT_SPEC, key);
     }
 
 
--- src/main/java/core/ItemsSeeker.java
+++ src/main/java/core/ItemsSeeker.java
@@ -1,16 +1,20 @@
 package core;
 
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+
 import java.util.Deque;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class ItemsSeeker {
 
-    private Condition condition;
-    private int maxThreads = 3;
-    private int itemsLimit = 60;
-
     private Logger logger;
+    private OkHttpClient client;
+    private final String BASE_URL = "https://svcs.ebay.com/services/search/FindingService/v1";
+    private final String appName;
+
+    private Condition condition = Condition.NOT_SPEC;
 
     private Deque<String> unprocessed = new ConcurrentLinkedDeque<>();
 
@@ -20,21 +24,36 @@ public class ItemsSeeker {
     private double avgPrice;
     private double avgSoldPrice;
 
-    public ItemsSeeker(List<String> queries, Condition condition) {
+    public ItemsSeeker(List<String> queries, Condition condition, String appname) {
         this.condition = condition;
         unprocessed.addAll(queries);
+        this.appName = appname;
     }
 
     private int threads;
 
 
 
+    private Headers.Builder baseHeadersBuilder() {
+        return new Headers.Builder()
+                .add("Host", "betslipapi.isppro.net")
+                ;
+    }
+
     private void log(String message) {
         if (logger != null) logger.log(message);
     }
 
+    public void setLogger(Logger logger) {
+        this.logger = logger;
+    }
+
     public enum Condition {
         NEW, USED, NOT_SPEC
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
     }
 
 }
